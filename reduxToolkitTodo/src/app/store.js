c6293b0c1d25f3801ddb92dd3d834ded99b7ae0a
Tushar <tusharbhatt.1405@gmail.com>
2025-08-01 14:54:29 +0530

    redux toolkit project

---
+++ reduxToolkitTodo/src/app/store.js
@@ -0,0 +1,7 @@
+// importing configure store
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+
+export const store = configureStore({
+    reducer: todoReducer
+});
